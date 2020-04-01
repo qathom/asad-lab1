8aa8b5f00d18d4612631b981fa5f338fa622efd0
ayrton <ayrtondumas@gmail.com>
2020-04-01 14:02:54 +0200

    my simple end to end socket.io app

---
+++ backend-vanilla/http-server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+// serve static file, exemple : html et js
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+module.exports = app;
