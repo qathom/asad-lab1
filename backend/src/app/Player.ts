23739018cfba45f199a1c226984c0102b678ee2b
Anthony Meizoso <ameizoso@logitech.com>
2020-05-21 13:48:36 +0200

    Lint

--- backend/src/app/Player.ts
+++ backend/src/app/Player.ts
@@ -1,19 +1,27 @@
 export class Player {
-    // Private id
-    // Private bank ?
-    id: string
-    password: string
-    bank: number
-    currentAmountBetted: number
+  // Private id
+  // Private bank ?
+  id: string
+  password: string
+  bank: number
+  currentAmountBetted: number
 
-    constructor(id: string, password: string, bank: number) {
-        this.id = id
-        this.password = password
-        this.bank = bank
-        this.currentAmountBetted = 0
+  constructor(id: string, password: string, bank: number) {
+    this.id = id
+    this.password = password
+    this.bank = bank
+    this.currentAmountBetted = 0
+  }
+
+  canBet(amount: number): boolean {
+    return (this.currentAmountBetted + amount) <= this.bank
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      password: this.password,
+      bank: this.bank,
     }
-
-    canBet(amount): Boolean{
-        return (this.currentAmountBetted + amount) <= this.bank
-    } 
+  }
 }
