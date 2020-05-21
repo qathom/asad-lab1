51024f01b7cccb885da092343b99cb9c767eee1b
Anthony Meizoso <ameizoso@logitech.com>
2020-05-21 13:46:48 +0200

    Added token in types

--- backend/types/index.ts
+++ backend/types/index.ts
@@ -1,21 +1,25 @@
 import { BetType } from "../src/app/utils/BetType";
 
+type TokenPayload = {
+  token: string,
+};
+
 export type ClientInitData = {
   playerId: string;
   playerPassword: string;
 };
 
-
-export type ClientAccountData = {
+export type ClientAccountData = TokenPayload & {
   playerId: string;
   playerPassword: string;
   playerBalance: number;
 };
 
-
-export type ClientBetData = {
+export type ClientBetData = TokenPayload & {
   playerId: string;
   cell: number;
   betType: BetType;
   amount: number;
 };
+
+export type VerifyTokenData = TokenPayload;
